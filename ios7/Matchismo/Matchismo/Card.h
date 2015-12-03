f551dbaaffe834d7e94d907ace50328c83867a5b
daviskoh <koh.davis.0@gmail.com>
2015-12-02 22:24:28 -0500

    add card model

---
+++ ios7/Matchismo/Matchismo/Card.h
@@ -0,0 +1,20 @@
+//
+//  Card.h
+//  Matchismo
+//
+//  Created by Davis Koh on 12/2/15.
+//  Copyright © 2015 com.DavisKoh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@property (strong, nonatomic) NSString *contents;
+
+// NOTE: only time to explicitly change getter names
+// is for BOOL properties
+@property (nonatomic, getter=isChosen) BOOL chosen;
+@property (nonatomic, getter=isMatched) BOOL matched;
+
+@end
---
+++ ios7/Matchismo/Matchismo/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Matchismo
+//
+//  Created by Davis Koh on 12/2/15.
+//  Copyright © 2015 com.DavisKoh. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
