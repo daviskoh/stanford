54c907838989abaeddc52a134bde919c31a5eb18
daviskoh <koh.davis.0@gmail.com>
2015-12-15 19:34:49 -0500

    add map method to nsarray

---
+++ ios7/Matchismo/Matchismo/NSArray+NSArray_KOHMap.h
@@ -0,0 +1,13 @@
+//
+//  NSArray+NSArray_KOHMap.h
+//  Matchismo
+//
+//  Created by Davis Koh on 12/15/15.
+//  Copyright © 2015 com.DavisKoh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (NSArray_KOHMap)
+
+@end
