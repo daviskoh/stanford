31ba0c9463fa4f0d57220a09a0acf0e83390f88b
daviskoh <koh.davis.0@gmail.com>
2015-12-17 00:35:21 -0500

    show high scores

--- ios7/Matchismo/Matchismo/HighScoresViewController.swift
+++ ios7/Matchismo/Matchismo/HighScoresViewController.swift
@@ -9,8 +9,36 @@
 import UIKit
 
 class HighScoresViewController: UIViewController {
-    override func loadView() {
-        self.view = UIView(frame: UIScreen.mainScreen().bounds)
+    override func viewDidLoad() {
         self.view.backgroundColor = UIColor.yellowColor()
+
+        let bounds = self.view.bounds
+        let textView = UITextView(frame: CGRect(
+            x: bounds.origin.x,
+            y: 20,
+            width: bounds.size.width,
+            height: bounds.size.height
+        ))
+
+        self.view.addSubview(self.textViewWithText(textView))
+    }
+
+    func textViewWithText(textView: UITextView) -> UITextView {
+        textView.text = self.createScoreString("Playing Card") + "\n"
+        textView.text = textView.text + self.createScoreString("Set Card")
+        textView.font = UIFont.systemFontOfSize(20)
+        return textView
+    }
+
+    func createScoreString(gameType: String) -> String {
+        let defaults = NSUserDefaults.standardUserDefaults()
+
+        var playingCardScore = "\(gameType) Game High Scores \n"
+        // FIXME: really ghetto keys...
+        for score in defaults.arrayForKey("\(gameType)HighScores")! {
+            playingCardScore += score.stringValue + "\n"
+        }
+
+        return playingCardScore
     }
 }
