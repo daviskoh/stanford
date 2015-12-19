88e4229da71daec79dd4cfd5ae77f4d4001665f8
daviskoh <koh.davis.0@gmail.com>
2015-12-19 16:14:54 -0500

    add abstract cardview class

--- ios7/Matchismo/Matchismo/CardView.h
+++ ios7/Matchismo/Matchismo/CardView.h
@@ -5,9 +5,16 @@
 //  Created by Davis Koh on 12/2/15.
 //  Copyright © 2015 com.DavisKoh. All rights reserved.
 //
+// Abstract Class
 
 #import <UIKit/UIKit.h>
 
-@interface CardView : UIButton
+@interface CardView : UIView
+
+@property (nonatomic) NSUInteger rank;
+@property (strong, nonatomic) NSString *suit;
+@property (nonatomic) BOOL faceUp;
+
+- (void)pinch:(UIPinchGestureRecognizer *)gesture;
 
 @end
--- ios7/Matchismo/Matchismo/CardView.m
+++ ios7/Matchismo/Matchismo/CardView.m
@@ -8,20 +8,185 @@
 
 #import "CardView.h"
 
+@interface CardView()
+
+@property (nonatomic) CGFloat faceCardScaleFactor;
+
+@end
+
 @implementation CardView
+
+#pragma mark - Getters & Setters
+
+// NOTE: setNeedsDisplay
+// - anyone changes prop,
+// tell system that view needs to be redrawn
+
+@synthesize faceCardScaleFactor = _faceCardScaleFactor;
+
+#define DEFAULT_FACE_CARD_SCALE_FACTOR 0.90
+
+- (CGFloat)faceCardScaleFactor {
+    if (!_faceCardScaleFactor) {
+        _faceCardScaleFactor = DEFAULT_FACE_CARD_SCALE_FACTOR;
+    }
+    return _faceCardScaleFactor;
+}
+
+- (void)setFaceCardScaleFactor:(CGFloat)faceCardScaleFactor {
+    _faceCardScaleFactor = faceCardScaleFactor;
+    [self setNeedsDisplay];
+}
+
+- (void)setSuit:(NSString *)suit {
+    _suit = suit;
+    [self setNeedsDisplay];
+}
+
+- (void)setRank:(NSUInteger)rank {
+    _rank = rank;
+    [self setNeedsDisplay];
+}
+
+- (void)setFaceUp:(BOOL)faceUp {
+    _faceUp = faceUp;
+    [self setNeedsDisplay];
+}
+
+#pragma mark - Gestures
+
+// adjust face card scale
+- (void)pinch:(UIPinchGestureRecognizer *)gesture {
+    if (gesture.state == UIGestureRecognizerStateChanged ||
+        gesture.state == UIGestureRecognizerStateEnded) {
+        self.faceCardScaleFactor *= gesture.scale;
+        gesture.scale = 1.0;
+    }
+}
+
+#pragma mark - Drawing
+
+#define CORNER_FONT_STANDARD_HEIGHT 180.0
+#define CORNER_RADIUS 12.0
+
+- (CGFloat)cornerScaleFactor {
+    return self.bounds.size.height / CORNER_FONT_STANDARD_HEIGHT;
+}
+
+- (CGFloat)cornerRadius {
+    return CORNER_RADIUS * [self cornerScaleFactor];
+}
+
+- (CGFloat)cornerOffset {
+    return [self cornerRadius] / 3.0;
+}
+
+- (void)drawRect:(CGRect)rect {
+    UIBezierPath *roundedRect = [UIBezierPath bezierPathWithRoundedRect:self.bounds
+                                                           cornerRadius:[self cornerRadius]];
+    [roundedRect addClip];
+
+    [[UIColor whiteColor] setFill];
+    // fills rect
+    UIRectFill(self.bounds);
+
+    [[UIColor blackColor] setStroke];
+    [roundedRect stroke];
+
+    if (self.faceUp) {
+
+        NSString *imageName = [NSString stringWithFormat:@"%@%@", [self rankAsString],self.suit];
+        UIImage *faceImage = [UIImage imageNamed:imageName];
+        if (faceImage) {
+            // prevent image from "smashing corners"
+            // scale image into rect
+            CGSize size = self.bounds.size;
+            CGRect imageRect = CGRectInset(self.bounds,
+                                           // 90% of card
+                                           size.width * (1.0 - self.faceCardScaleFactor),
+                                           size.height * (1.0 - self.faceCardScaleFactor)
+                                           );
+            [faceImage drawInRect:imageRect];
+        } else {
+            [self drawPips];
+        }
+
+        [self drawCorners];
+    } else {
+        [[UIImage imageNamed:@"cardback"] drawInRect:self.bounds];
+    }
+}
+
+// TODO: implement drawPips for non-face cards
+- (void)drawPips { // abstract
+    // very long implentation from lecture
+}
+
+- (NSString *)rankAsString {
+    return @[
+             @"?",
+             @"A",
+             @"2",
+             @"3",
+             @"4",
+             @"5",
+             @"6",
+             @"7",
+             @"8",
+             @"9",
+             @"10",
+             @"J",
+             @"Q",
+             @"K"
+             ][self.rank];
+}
+
+- (void)drawCorners {
+    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+    paragraphStyle.alignment = NSTextAlignmentCenter;
+
+    UIFont *cornerFont = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    cornerFont = [cornerFont fontWithSize:cornerFont.pointSize * [self cornerScaleFactor]];
+
+    NSString *string = [NSString stringWithFormat:@"%@\n%@", [self rankAsString], self.suit];
+    NSAttributedString *cornerText = [[NSAttributedString alloc] initWithString:string
+                                                                     attributes:@{
+                                                                                  NSFontAttributeName: cornerFont,
+                                                                                  NSParagraphStyleAttributeName: paragraphStyle
+                                                                                  }];
+    // top
+    CGRect textBounds;
+    textBounds.origin = CGPointMake(
+                                    [self cornerOffset],
+                                    [self cornerOffset]
+                                    );
+    textBounds.size = cornerText.size;
+    [cornerText drawInRect:textBounds];
+
+    // upside down
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextTranslateCTM(
+                          context,
+                          self.bounds.size.width,
+                          self.bounds.size.height
+                          );
+    CGContextRotateCTM(context, M_PI);
+    [cornerText drawInRect:textBounds];
+}
+
+- (void)setup {
+    self.backgroundColor = nil;
+    self.opaque = NO;
+    self.contentMode = UIViewContentModeRedraw;
+}
+
+#pragma mark - Init
 
 - (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
 
     if (self) {
-        UIImage *btnImage = [UIImage imageNamed:@"cardback.png"];
-        [self setBackgroundImage:btnImage
-                        forState:UIControlStateNormal];
-        //[self setTranslatesAutoresizingMaskIntoConstraints:NO];
-
-        [self setTitleColor:[UIColor blackColor]
-                   forState:UIControlStateNormal];
-        self.titleLabel.font = [UIFont systemFontOfSize:25];
+        [self setup];
     }
 
     return self;
