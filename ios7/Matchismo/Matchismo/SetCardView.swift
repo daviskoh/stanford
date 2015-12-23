50d1806ea90547d56059f06c488d2ac335f39bf7
daviskoh <koh.davis.0@gmail.com>
2015-12-23 14:20:46 -0400

    draw correct diamond

--- ios7/Matchismo/Matchismo/SetCardView.swift
+++ ios7/Matchismo/Matchismo/SetCardView.swift
@@ -9,8 +9,45 @@
 import UIKit
 
 class SetCardView: CardView {
-    func drawTriangle() {
+    func drawDiamond() {
+        let diamondPath = UIBezierPath()
 
+        let origin = CGPoint(
+            x: CGRectGetMidX(self.bounds),
+            y: CGRectGetMidY(self.bounds)
+        )
+        let dimensions = CGSize(
+            width: 10.0,
+            height: 5.0
+        )
+        let startingPoint = CGPointMake(origin.x, origin.y)
+
+        diamondPath.moveToPoint(startingPoint)
+
+        // Right/Down
+        diamondPath.addLineToPoint(CGPoint(
+            x: origin.x + dimensions.width,
+            y: origin.y + dimensions.height
+        ))
+
+        // Left/Down
+        diamondPath.addLineToPoint(CGPoint(
+            x: origin.x,
+            y: origin.y + (2.0 * dimensions.height)
+        ))
+
+        // Left/Up
+        diamondPath.addLineToPoint(CGPoint(
+            x: origin.x - dimensions.width,
+            y: origin.y + dimensions.height
+        ))
+
+        diamondPath.closePath()
+
+        self.color.setFill()
+        diamondPath.fill()
+        self.color.setStroke()
+        diamondPath.stroke()
     }
 
     override func drawRect(rect: CGRect) {
@@ -18,8 +55,8 @@ class SetCardView: CardView {
 
         if self.faceUp {
             switch self.suit {
-            case "triangle":
-                break
+            case "diamond":
+                self.drawDiamond()
             case "oval":
                 break
             case "squiggle":
