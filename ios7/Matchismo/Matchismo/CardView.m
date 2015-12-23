5d67d24d51cad541f62eb0a6fe65a852b339f530
daviskoh <koh.davis.0@gmail.com>
2015-12-22 20:13:03 -0400

    draw suit in color / implement drawpips

--- ios7/Matchismo/Matchismo/CardView.m
+++ ios7/Matchismo/Matchismo/CardView.m
@@ -120,6 +120,28 @@
 // TODO: implement drawPips for non-face cards -[-NEXT-]-
 - (void)drawPips { // abstract
     // very long implentation from lecture
+    // honestly...opt for just displaying the attributed text in
+    // proper color
+    // this is not a fucking drawing class...
+
+    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+    paragraphStyle.alignment = NSTextAlignmentCenter;
+
+    UIFont *middleFont = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    middleFont = [middleFont fontWithSize:(self.bounds.size.width / 2)];
+
+    NSAttributedString *middleText = [[NSAttributedString alloc] initWithString:self.suit
+                                                                     attributes:@{
+                                                                                  NSFontAttributeName: middleFont,
+                                                                                  NSParagraphStyleAttributeName: paragraphStyle,
+                                                                                  NSForegroundColorAttributeName: self.color
+                                                                                  }];
+    CGRect textBounds;
+    textBounds.size = middleText.size;
+    textBounds.origin.x = CGRectGetMidX(self.bounds) - (textBounds.size.width / 2);
+    textBounds.origin.y = CGRectGetMidY(self.bounds) - (textBounds.size.height / 2);
+
+    [middleText drawInRect:textBounds];
 }
 
 - (NSString *)rankAsString {
