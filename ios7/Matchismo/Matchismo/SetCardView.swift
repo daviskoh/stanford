86e0d7760c1e93890c7fe9f161d6f9da4c2b4fa1
daviskoh <koh.davis.0@gmail.com>
2015-12-23 00:03:23 -0400

    do-over

--- ios7/Matchismo/Matchismo/SetCardView.swift
+++ ios7/Matchismo/Matchismo/SetCardView.swift
@@ -9,53 +9,24 @@
 import UIKit
 
 class SetCardView: CardView {
-    func drawShape(suit: String) {
-        let length = self.bounds.size.width / 3
-        let bounds = CGRect(
-            x: self.bounds.origin.x,
-            y: self.bounds.origin.y,
-            width: length,
-            height: length
-        )
+    func drawTriangle() {
 
-        var shape: UIBezierPath?
-
-        if suit == "triangle" {
-            shape = UIBezierPath(
-                roundedRect: bounds,
-                cornerRadius: 0
-            )
-        } else if suit == "squiggle" {
-
-        } else if suit == "oval" {
-
-        }
-
-        shape?.addClip()
-        self.color.setFill()
-        UIRectFill(self.bounds)
-        UIColor.blackColor().setStroke()
-        shape?.stroke()
-
-        let context = UIGraphicsGetCurrentContext()
-        CGContextTranslateCTM(
-            context,
-            bounds.size.width,
-            bounds.size.height
-        )
-        CGContextRotateCTM(
-            context,
-            CGFloat(M_PI*45/180)
-        )
-
-        self.drawRect(bounds)
     }
 
     override func drawRect(rect: CGRect) {
         super.drawRect(rect)
 
         if self.faceUp {
-            self.drawShape(self.suit)
+            switch self.suit {
+            case "triangle":
+                break
+            case "oval":
+                break
+            case "squiggle":
+                break
+            default:
+                break
+            }
         }
     }
 }
