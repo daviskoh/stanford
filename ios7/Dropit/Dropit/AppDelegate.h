48fa1d94034bc21377ed3b351b15761e720658c1
daviskoh <koh.davis.0@gmail.com>
2015-12-27 21:45:28 -0500

    init Dropit demo

---
+++ ios7/Dropit/Dropit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dropit
+//
+//  Created by Davis Koh on 12/27/15.
+//  Copyright © 2015 com.DavisKoh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios7/Dropit/Dropit/DropitViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Dropit
+//
+//  Created by Davis Koh on 12/27/15.
+//  Copyright © 2015 com.DavisKoh. All rights reserved.
+//
+
+#import "DropitViewController.h"
+
+@interface DropitViewController ()
+
+@end
+
+@implementation DropitViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
