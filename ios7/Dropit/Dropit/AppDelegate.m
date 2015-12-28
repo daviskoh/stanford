bf57e88cd0c778486dc968c120ab8460e22a558c
daviskoh <koh.davis.0@gmail.com>
2015-12-27 22:00:56 -0500

    add simple subview w/ tap gesture

--- ios7/Dropit/Dropit/AppDelegate.m
+++ ios7/Dropit/Dropit/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "DropitViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+
+    UIWindow *window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    window.rootViewController = [[DropitViewController alloc] init];
+    self.window = window;
+
+    [self.window makeKeyAndVisible];
+
     return YES;
 }
 
--- ios7/Dropit/Dropit/DropitViewController.m
+++ ios7/Dropit/Dropit/DropitViewController.m
@@ -10,18 +10,51 @@
 
 @interface DropitViewController ()
 
+@property (nonatomic) UIView *gameView;
+
 @end
 
 @implementation DropitViewController
 
+static const CGSize DROP_SIZE = { 40, 40 };
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.view.backgroundColor = [UIColor whiteColor];
+
+    self.gameView = [[UIView alloc] initWithFrame:self.view.frame];
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                          action:@selector(tap:)];
+    [self.gameView addGestureRecognizer:tapGesture];
+    [self.view addSubview:self.gameView];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)tap:(UITapGestureRecognizer *)sender {
+    [self drop];
+}
+
+- (void)drop {
+    CGRect frame;
+    frame.origin = CGPointZero;
+    frame.size = DROP_SIZE;
+    int x = (arc4random()%(int)self.gameView.bounds.size.width) / DROP_SIZE.width;
+    frame.origin.x = x * DROP_SIZE.width;
+
+    UIView *dropView = [[UIView alloc] initWithFrame:frame];
+    dropView.backgroundColor = [self randomColor];
+    [self.gameView addSubview:dropView];
+}
+
+- (UIColor *)randomColor {
+    switch (arc4random()%5) {
+        case 0: return [UIColor greenColor];
+        case 1: return [UIColor blueColor];
+        case 2: return [UIColor orangeColor];
+        case 3: return [UIColor redColor];
+        case 4: return [UIColor purpleColor];
+    }
+    return [UIColor blackColor];
 }
 
 @end
