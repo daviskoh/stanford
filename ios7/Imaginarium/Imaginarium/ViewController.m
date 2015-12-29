90bbb0c5f640dc11a4fe78aa59e81296d5742efa
daviskoh <koh.davis.0@gmail.com>
2015-12-29 16:57:50 -0500

    add centered buttons

--- ios7/Imaginarium/Imaginarium/ViewController.m
+++ ios7/Imaginarium/Imaginarium/ViewController.m
@@ -16,6 +16,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    self.view.backgroundColor = [UIColor whiteColor];
+
+    CGFloat offset = -100;
+    NSArray *buttons = @[@"Flower", @"Peppers", @"Jellyfish"];
+    for (NSString *buttonTitle in buttons) {
+        UIButton *button = [[UIButton alloc] init];
+        [button setTitle:buttonTitle forState:UIControlStateNormal];
+        [button setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+        [button.titleLabel setTextAlignment:NSTextAlignmentCenter];
+        [button sizeToFit];
+
+        CGRect frame = button.frame;
+        frame.origin.x = CGRectGetMidX(self.view.bounds) - (button.frame.size.width / 2);
+        frame.origin.y = CGRectGetMidY(self.view.bounds) + offset;
+        offset += 100;
+        button.frame = frame;
+
+        [self.view addSubview:button];
+    }
 }
 
 @end
