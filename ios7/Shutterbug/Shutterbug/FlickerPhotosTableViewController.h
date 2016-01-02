ae2ccb969bdfd3d1256d8f19817e1f409757597a
daviskoh <koh.davis.0@gmail.com>
2016-01-02 18:14:49 -0500

    init Shutterbug demo: lecture 11

---
+++ ios7/Shutterbug/Shutterbug/FlickerPhotosTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FlickerPhotosTableViewController.h
+//  Shutterbug
+//
+//  Created by Davis Koh on 1/2/16.
+//  Copyright © 2016 com.DavisKoh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlickerPhotosTableViewController : UITableViewController
+
+@end
