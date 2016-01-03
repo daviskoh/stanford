bb704a2ef73cd66c0a93cf1a1a0f43d6b4ad0481
daviskoh <koh.davis.0@gmail.com>
2016-01-03 14:22:07 -0500

    init core data ex

---
+++ ios7/CoreDataExample/CoreDataExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreDataExample
+//
+//  Created by Davis Koh on 1/3/16.
+//  Copyright © 2016 com.DavisKoh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios7/CoreDataExample/CoreDataExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreDataExample
+//
+//  Created by Davis Koh on 1/3/16.
+//  Copyright © 2016 com.DavisKoh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
