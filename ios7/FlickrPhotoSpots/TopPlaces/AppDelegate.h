c8e57ca5f068daa33ee304b35bc2a9a8e9e6522b
daviskoh <koh.davis.0@gmail.com>
2016-01-16 15:52:25 -0500

    init FlickrPhotoSpots

---
+++ ios7/FlickrPhotoSpots/TopPlaces/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TopPlaces
+//
+//  Created by Davis Koh on 1/16/16.
+//  Copyright © 2016 com.DavisKoh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
